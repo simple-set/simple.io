5ae7b83a1c148499e1ab7c76b9a02f6e5725da1e
xk <xukui44@foxmail.com>
2024-03-27 19:08:43 +0800

    body ReadBytes

--- src/protocol/simpleHttp/body.go
+++ src/protocol/simpleHttp/body.go
@@ -69,6 +69,25 @@ func (b *Body) Read(p []byte) (n int, err error) {
 	return n, nil
 }
 
+func (b *Body) ReadBytes() ([]byte, error) {
+	if b.size > 0 {
+		buff := make([]byte, b.size)
+		if _, err := b.Read(buff); err != nil {
+			return nil, err
+		}
+		return buff, nil
+	}
+	return nil, errors.New("")
+}
+
+func (b *Body) ReadString() (string, error) {
+	if readBytes, err := b.ReadBytes(); err != nil {
+		return string(readBytes), nil
+	} else {
+		return "", err
+	}
+}
+
 func NewReadBody(size int64, buff *codec.ByteBuf) *Body {
 	if buff.ReadBuffer() != nil {
 		return &Body{size: size, buff: buff}
